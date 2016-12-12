dddb6f71371a4c5ffe1752acce2c7c42da19002c
luantu <luantu@vip.qq.com>
2016-12-12 10:49:07 +0800

    add some script

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-apt-get install zsh && \
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc && \
+#!/bin/bash
+
+IS_ZSH_INSTALL=`dpkg -l | grep zsh | wc -l`
+CURRENT_SHELL=`ps | grep $$ | awk '{print $4}'`
+
+if [ ${IS_ZSH_INSTALL} -eq 0 ]; then
+    echo "zsh not install..."
+    apt-get install zsh
+fi
+
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
 chsh -s /bin/zsh
 
--- web-deploy.sh
+++ web-deploy.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 #file_name: rg-deploy.sh
 
+UPGRADE_PACKAGE="/root/odl_test/RG-ONC_2.1.0-Web.tar.gz"
+
 export depoly_ip=`echo  $SSH_CONNECTION | awk '{print $3}'` 
 
 # kill tomcat
 export tomcatpid=`ps -ef | grep tomcat | grep -v grep | awk '{print $2}'` && \
 [ -n "$tomcatpid" ] && \
 kill -9 ${tomcatpid} && \
-sleep 5 &&  \
-ps -ef | grep tomcat | grep -v grep
+sleep 5
 
-# download tar.gz
-read -p "Enter web.tar.gz url:" weburl 
-CURRENTDIR=${PWD}
-cd /root/odl_test/ && wget ${weburl}
+WEB_PROCESS_CNT=`ps -ef | grep tomcat | grep -v grep | wc -l`
+if [ ${WEB_PROCESS_CNT} -eq 0 ]; then
+    echo "stop tomcat success... "
+fi
+
+if [ ! -f "${UPGRADE_PACKAGE}" ]; then  
+    # download tar.gz
+    read -p "Enter web.tar.gz url:" weburl 
+    CURRENTDIR=${PWD}
+    cd /root/odl_test/ && wget ${weburl}
+fi
 
 # upgrade
+echo "upgrade new version ... "
 rm -rf /root/odl_test/web/ && \
 tar -xf /root/odl_test/RG-ONC_2.1.0-Web.tar.gz && \
 mv /root/odl_test/RG-ONC_2.1.0-Web /root/odl_test/web && \
