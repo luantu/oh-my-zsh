45d2f359def56e62dfeb771f525df62828137490
luantu <luantu@vip.qq.com>
2017-03-21 19:40:05 +0800

    update

--- getVersion.sh
+++ getVersion.sh
@@ -16,7 +16,70 @@ REMOTE_URL=${REMOTE_URL}${FILE_NAME}
 
 echo "get url ${REMOTE_URL}"
 
-echo ${REMOTE_URL} > /root/rgonc_version
+echo ${REMOTE_URL} > /root/"$1""_version"
 
 #echo "get url ${REMOTE_URL}"
 #wget ${REMOTE_URL} -O /tmp/${FILE_NAME}
+
+#!/bin/bash
+
+#if [ "$1"x == x ]; then
+#    echo "getVersion.sh {dc|campus|rgonc}"
+#    exit
+#fi
+
+get_web_version() {
+    REMOTE_URL=http://192.168.5.14/ngcf/output/sdn_UI/$1/
+    wget -q ${REMOTE_URL} -O /tmp/514tmp
+    REMOTE_DIR=`cat /tmp/514tmp | grep "onc_UI" | head -n 1 | awk -F 'href' '{print $2}' | awk -F '"' '{print $2}'`
+    REMOTE_URL=${REMOTE_URL}${REMOTE_DIR}
+    wget -q ${REMOTE_URL} -O /tmp/514tmp
+
+    FILE_NAME=`cat /tmp/514tmp | grep Web.tar.gz | awk -F 'href' '{print $2}' | awk -F '"'  '{print $2}'`
+    REMOTE_URL=${REMOTE_URL}${FILE_NAME}
+
+    echo "get url ${REMOTE_URL}"
+
+    echo ${REMOTE_URL} > /root/"$2""_web_version"
+
+    #echo "get url ${REMOTE_URL}"
+    #wget ${REMOTE_URL} -O /tmp/${FILE_NAME}
+
+}
+
+WEB_DIR=""
+PROJECT=""
+case  "$1"  in
+    dc_1.1_tags)
+        get_web_version RGONC-LS-DC-CLOUD $1
+        ;;   
+
+    campus_1.10)
+        get_web_version RGONC-CAMPUS-CLOUD $1
+        ;;
+
+    rgonc_2.2.0)
+        get_web_version SDN_NFV_web_UI $1
+        ;;
+
+    esac
+
+get_web_version() {
+    REMOTE_URL=http://192.168.5.14/ngcf/output/sdn_UI/$1/
+    wget -q ${REMOTE_URL} -O /tmp/514tmp
+    REMOTE_DIR=`cat /tmp/514tmp | grep "onc_UI" | head -n 1 | awk -F 'href' '{print $2}' | awk -F '"' '{print $2}'`
+    REMOTE_URL=${REMOTE_URL}${REMOTE_DIR}
+    wget -q ${REMOTE_URL} -O /tmp/514tmp
+
+    FILE_NAME=`cat /tmp/514tmp | grep Web.tar.gz | awk -F 'href' '{print $2}' | awk -F '"'  '{print $2}'`
+    REMOTE_URL=${REMOTE_URL}${FILE_NAME}
+
+    echo "get url ${REMOTE_URL}"
+
+    echo ${REMOTE_URL} > /root/"$2""_web_version"
+
+    #echo "get url ${REMOTE_URL}"
+    #wget ${REMOTE_URL} -O /tmp/${FILE_NAME}
+
+}
+
--- web-deploy.sh
+++ web-deploy.sh
@@ -16,19 +16,22 @@ if [ ${WEB_PROCESS_CNT} -eq 0 ]; then
     echo "stop tomcat success... "
 fi
 
-if [ ! -f "${UPGRADE_PACKAGE}" ]; then  
+#if [ ! -f "${UPGRADE_PACKAGE}" ]; then  
     # download tar.gz
-    read -p "Enter web.tar.gz url:" weburl 
-    CURRENTDIR=${PWD}
-    cd /root/odl_test/ && wget ${weburl}
-fi
+#    read -p "Enter web.tar.gz url:" weburl 
+#    CURRENTDIR=${PWD}
+#    cd /root/odl_test/ && wget ${weburl}
+#fi
+
+WEB_URL=`cat /root/rgonc_web_version`
+cd /root/odl_test/ && wget ${WEB_URL}
 
 # upgrade
 echo "upgrade new version ... "
 rm -rf /root/odl_test/web/ && \
-tar -xf /root/odl_test/RG-ONC_2.1.0-Web.tar.gz && \
-mv /root/odl_test/RG-ONC_2.1.0-Web /root/odl_test/web && \
-rm /root/odl_test/RG-ONC_2.1.0-Web.tar.gz && \
+tar -xf /root/odl_test/RG-ONC_2.2.0-Web.tar.gz && \
+mv /root/odl_test/RG-ONC_2.2.0-Web /root/odl_test/web && \
+rm /root/odl_test/RG-ONC_2.2.0-Web.tar.gz && \
 chmod +x /root/odl_test/web/bin/* && ./web/bin/startFirst.sh ${depoly_ip} && /root/odl_test/web/bin/https.sh ${depoly_ip}
 
 # start tomcat
